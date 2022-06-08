d2a1fe2d854a80751e9b881e554a13d11f8ca6e5
Shabelnikova Margarita <lovelymargov@gmail.com>
2022-06-08 07:23:04 +0600

    operates

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -6,11 +6,35 @@
 // let firstName = 'Jonas';
 // console.log(firstName);
 
-let jsIsFun = true;
-console.log(jsIsFun);
-console.log(typeof true);
-jsIsFun = 'janas';
-console.log(typeof jsIsFun);
+// let jsIsFun = true;
+// console.log(jsIsFun);
+// console.log(typeof true);
+// jsIsFun = 'janas';
+// console.log(typeof jsIsFun);
 
-let year;
-console.log(year);
+// let year;
+// console.log(year);
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageJonas, ageSarah);
+
+console.log(ageJonas * 2, ageJonas / 10, 2**3);
+
+const firstName = 'Jonas';
+const lastName = 'Shmidt';
+console.log(firstName + lastName);
+
+let x = 10 + 5;
+x += 10;
+x *= 4;
+x++;
+x--;
+console.log(x);
+
+console.log(ageJonas > ageSarah);
+console.log(ageSarah >= 18);
+
+const isFullAge = ageSarah >= 18;
+
+console.log(now - 1991 > now - 2018);
