ac6562703d8e40b6c336ce7103326ad1f5594a5c
Shabelnikova Margarita <lovelymargov@gmail.com>
2022-06-08 06:25:09 +0600

    type data

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -1,7 +1,16 @@
-let js = 'amazing';
-console.log(40 + 80 + 2);
-console.log('Jonas');
-console.log(23);
+// let js = 'amazing';
+// console.log(40 + 80 + 2);
+// console.log('Jonas');
+// console.log(23);
 
-let firstName = 'Jonas';
-console.log(firstName);
+// let firstName = 'Jonas';
+// console.log(firstName);
+
+let jsIsFun = true;
+console.log(jsIsFun);
+console.log(typeof true);
+jsIsFun = 'janas';
+console.log(typeof jsIsFun);
+
+let year;
+console.log(year);
